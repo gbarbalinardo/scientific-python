27335592cf70ce31d84704484ae2deb4a4643bda
Giuseppe Barbalinardo <giuseppp@gmail.com>
2019-05-13 19:45:44 -0700

    Update rk_integrator.py

--- rk_integrator.py
+++ rk_integrator.py
@@ -2,38 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import complex_ode, ode
 
-def fun(t, y):
+hbar = 1.0
+
+def hamiltonian(t=0):
+    omega = 1.0
+    chi = 0.1
+    return np.array([[omega,  chi], [chi, - omega]])
+
+def rhs(t, psi):
     """
     t	(float) Current time.
     z	(ndarray) Current variable values.
     """
-
-    omega = 2 * np.pi
-    chi = 10.
-    psi = [1j *  omega * y[0] - chi * y[1], chi * y[0] -  omega * y[1]]
-    return psi
+    return - 1.0j /hbar * hamiltonian().dot(psi)
 
 # Create an `ode` instance to solve the system of differential
-# equations defined by `fun`, and set the solver method to dopri5 (an alternative more precise RK-8 is dop853)
-solver = complex_ode(fun)
+# equations defined by `hamiltonian`, and set the solver method to dopri5 (an alternative more precise RK-8 is dop853)
+solver = complex_ode(rhs)
 solver.set_integrator('dopri5')
 
 # Give the value of omega to the solver. This is passed to
-# `fun` when the solver calls it.
+# `hamiltonian` when the solver calls it.
 
 # Set the initial value z(0) = z0.
 t0 = 0.0
-z0 = np.array([1, -0.25]).astype(np.complex)
-solver.set_initial_value(z0, t0)
+population_0 = 0.1
+population_1 = np.sqrt(1 - population_0 ** 2)
+psi0 = np.array([population_0, population_1]).astype(np.complex)
+solver.set_initial_value(psi0, t0)
 
 # Create the array `t` of time values at which to compute
 # the solution, and create an array to hold the solution.
 # Put the initial value in the solution array.
-t1 = 2.5
-N = 75
+t1 = 10
+N = 100
 times = np.linspace(t0, t1, N)
-sol = np.zeros((N, 2)).astype(np.complex)
-sol[0] = z0
+psi = np.zeros((N, 2)).astype(np.complex)
+psi[0] = psi0
 
 # Repeatedly call the `integrate` method to advance the
 # solution to time t[k], and save the solution in sol[k].
@@ -41,11 +46,14 @@ for i in range(1, times.shape[0]):
     t = times[i]
     if not solver.successful():
         break
-    sol[i] = solver.integrate(t)
+    psi[i] = solver.integrate(t)
 
 # Plot the solution...
-plt.plot(times, sol[:,0].real, label='x')
-plt.plot(times, sol[:,1].real, label='y')
+plt.plot(times, psi[:,0].real, label='psi_0_real')
+plt.plot(times, psi[:,0].imag, label='psi_0_imag')
+plt.plot(times, psi[:,1].real, label='psi_1_real')
+plt.plot(times, psi[:,1].imag, label='psi_1_imag')
+plt.plot(times, psi.dot(psi.T.conj()).diagonal().real, label='normalization')
 plt.xlabel('t')
 plt.grid(True)
 plt.legend()
