f2641422771c1326cf779181e7902ac28c36f30d
Giuseppe Barbalinardo <giuseppe.barbalinardo@gmail.com>
2019-05-14 19:53:15 -0700

    Minor code cleanup

--- how_to_make_a_gif.py
+++ how_to_make_a_gif.py
@@ -4,15 +4,15 @@ from matplotlib import animation
 
 def generate_plot(t):
     '''animation function. This is called sequentially for every time t'''
-    
+
     # remove the previous frame, if any
     fig.gca().remove()
-    
+
     # now plot
     x = np.linspace(0., 1., 100)
     y = np.sin(2. * np.pi * (x - t))
     return plt.plot(x, y)
-    
+
 # create an empty figure
 fig = plt.figure ()
 
@@ -21,8 +21,7 @@ times = np.linspace(0., 5., 200.)
 
 # render the animation
 anim = animation.FuncAnimation(fig, generate_plot, frames=times, blit=True)
-                    
-# save, install ffmpeg with `sudo conda install -c conda-forge ffmpeg`
+
+# save, install ffmpeg with  conda install -c conda-forge ffmpeg
 writer = animation.writers['ffmpeg'](fps=20)
 anim.save('animation.mp4', writer=writer)
-
